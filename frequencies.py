5dc217150bb44a6d6fdc8a592aff0830f551b8bf
Kota Amemiya <kotaamemiya@gmail.com>
2023-10-05 01:02:14 +0100

    Add frequencies method.

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,13 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        item = str(item)
+        if item in frequencies:
+            frequencies[item] = frequencies[item] + 1
+        else:
+            frequencies[item] = 1
     return frequencies
+
+inputs = input("Enter to get things sorted: ")
+print(frequencies(inputs))
